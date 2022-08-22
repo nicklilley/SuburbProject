4d050e39f76dffc022b36a449267b910c0f3bef4
Nick <nicholas.lilleyman@gmail.com>
2022-08-22 16:29:53 +0200

    NickL/refactored repo folder structure

--- extract_and_load/__init__.py
+++ extract_and_load/__init__.py
@@ -1,5 +1,5 @@
 import boto3
-from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from extract_and_load.config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 client = boto3.client(
 aws_access_key_id=AWS_ACCESS_KEY_ID,
 aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
