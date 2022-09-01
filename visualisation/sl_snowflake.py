b3d9524d412fc5c9d9fcf08e7b2b2c1d177491e8
Nick <nicholas.lilleyman@gmail.com>
2022-09-01 17:48:47 +0200

    NickL/Connected Streamlit to Snowflake account

---
+++ visualisation/sl_snowflake.py
@@ -0,0 +1,27 @@
+# streamlit_app.py
+
+import streamlit as st
+import pandas as pd
+import snowflake.connector
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(**st.secrets["snowflake"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from FCT_SUBURB_DEMOGRAPHICS;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
