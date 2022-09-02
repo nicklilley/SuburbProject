e7474054d31a2290ef3e81e3708d6c210efe21fa
Nick <nicholas.lilleyman@gmail.com>
2022-09-02 18:27:03 +0200

    NickL/Added marks, tooltip, working filter

--- visualisation/sl_snowflake_df_filter.py
+++ visualisation/sl_snowflake_df_filter.py
@@ -12,27 +12,10 @@ import altair as alt
 @st.experimental_singleton
 def init_connection():
     return snowflake.connector.connect(**st.secrets["snowflake"])
-
 ctx = init_connection()
 
 # Create a cursor object.
 cur = ctx.cursor()
-
-
-
-add_radio2 = st.sidebar.radio(
-    "How sick is Jarrad",
-    ("Not very", "Some", "A lot")
-)
-
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Pidegon", "Rat", "Mobile phone")
-)
-
-
-
 
 # Execute a statement that will generate a result set.
 sql = ("""
@@ -44,27 +27,52 @@ dim_date_sk
 FROM ANALYTICS.DBT_NLILLEYMAN_COMMON.FCT_SUBURB_REALTY_PERFORMANCE PERF
 LEFT JOIN ANALYTICS.DBT_NLILLEYMAN_COMMON.DIM_SUBURB_GEOGRAPHY GEO on GEO.dim_suburb_sk = PERF.dim_suburb_sk
 WHERE metric = 'medianSoldPrice'
-AND suburb = 'Willetton'
+AND suburb IN ('Willetton', 'Harrisdale')
 ORDER BY dim_date_sk ASC
  """)
-
 cur.execute(sql)
 
 # Fetch the result set from the cursor and deliver it as the Pandas DataFrame.
 df = cur.fetch_pandas_all()
 df.set_index('DIM_DATE_SK')
-df['DIM_DATE_SK']=pd.to_datetime(df['DIM_DATE_SK'])
+#df['DIM_DATE_SK']=pd.to_datetime(df['DIM_DATE_SK'])
+df['VALUE']=pd.to_numeric(df['VALUE'])
+
+
+#############FILTERS###########
+#Get unique values for a column inside the dataframe
+unique_suburbs = df['SUBURB'].unique()
+
+# Using object notation
+select_suburbs = st.sidebar.multiselect(
+    "Where you live?",
+    (unique_suburbs),
+    default=["Willetton"]
+)
+
+#Filters data frame based on selected states
+df = df[df.SUBURB.isin(select_suburbs)]
+
 
 st.write(df)
-st.line_chart(df, x='DIM_DATE_SK', y='VALUE')
-# ...
 
-#
-#chart = alt.Chart(df).mark_line().encode(
-#    x='DIM_DATE_SK',
-#    y='VALUE',
-#    color='SUBURB',
-#    strokeDash='SUBURB',
-#)
-#st.altair_chart(chart)
-#
+##### PLOT CHART #####
+chart = alt.Chart(df).mark_line(
+    
+    point={
+        "filled": False,
+        "fill": "white",
+        "tooltip": True
+        }
+).encode(
+    alt.X('DIM_DATE_SK',
+        axis=alt.Axis(title='Date', grid=False)),
+    alt.Y('VALUE',
+        axis=alt.Axis(title='Value', grid=False)),
+    color='SUBURB',
+    #strokeDash='SUBURB',
+    opacity=alt.value(0.8)
+)
+st.altair_chart(chart, use_container_width=True)
+
+st.line_chart(df)
